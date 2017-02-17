9adb7a25106b9669a40329d691a75af18afc9b34
sarahemilym <sarahemendham@gmail.com>
2017-02-17 17:42:04 +0000

    added logout and basic navbar for testing

---
+++ src/js/controllers/main.controller.js
@@ -0,0 +1,22 @@
+angular
+  .module('MuSync')
+  .controller('MainCtrl', MainCtrl);
+
+
+MainCtrl.$inject = ['$rootScope', 'CurrentUserService', '$state'];
+function MainCtrl($rootScope, CurrentUserService, $state) {
+  const vm = this;
+
+  vm.logout = () => {
+    CurrentUserService.removeUser();
+    $state.go('home');
+  };
+
+  $rootScope.$on('loggedIn', () => {
+    vm.user = CurrentUserService.currentUser;
+  });
+
+  $rootScope.$on('loggedOut', () => {
+    vm.user = null;
+  });
+}
