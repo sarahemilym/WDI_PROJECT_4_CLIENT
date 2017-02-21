e09471ac7277252c46c72538c33052815fa956a3
sarahemilym <sarahemendham@gmail.com>
2017-02-21 15:06:04 +0000

    can create playlists

--- src/js/controllers/rooms/show.controller.js
+++ src/js/controllers/rooms/show.controller.js
@@ -13,8 +13,13 @@ function RoomsShowCtrl(API, $stateParams, User, Room, CurrentUserService, $state
 
   vm.authorize = authorize;
   vm.createPlaylist = createPlaylist;
+  vm.searchArtists = searchArtists;
+  vm.searchTracks = searchTracks;
+  vm.addToPlaylist = addToPlaylist;
   vm.playlistCreated = false;
   vm.authorized = false;
+  vm.searchstatusArtists = false;
+  vm.searchstatusTracks = false;
 
   Room.get($stateParams, (data) => {
     CurrentUserService.getUser();
@@ -62,24 +67,67 @@ function RoomsShowCtrl(API, $stateParams, User, Room, CurrentUserService, $state
       $http
       .post(url, parameter, vm.token)
       .then(function(data) {
-        // console.log('playlist', data)
+        console.log('playlist', data.data)
         vm.playlist = data.data;
+        vm.playlistid = data.data.id;
       });
       // .err(err => {
       //   console.log(err);
       // });
     }
-    //   console.log('working', response);
-    // })
-    // .catch(function(response) {
-    //   console.log('something went wrong', response);
-    // });
-  // }
 
+    function searchArtists() {
+      vm.searchstatusArtists = false;
+      const text = angular.element(document.querySelector('#searchArtist-text')).val();
 
+      $http
+        .get('https://api.spotify.com/v1/search?q='+text+'&type=artist')
+        .then(response => {
+        vm.results = response.data.artists.items;
 
-    // e.target.setAttribute('disabled', true);
-    // angular.element(document.querySelector('#createPlaylist')).isDisabled = true;
+        vm.searchstatusArtists = true;
+        vm.searchstatusTracks = false;
+        angular.element(document.querySelector('#searchArtist-text')).val('');
+        console.log('search result', vm.results);
+      });
+    }
 
+    function searchTracks(){
+      vm.searchstatus = false;
+      const text = angular.element(document.querySelector('#searchArtists-text')).val();
+      $http({
+        method: 'GET',
+        url: 'https://api.spotify.com/v1/search?q='+text+'&type=track'
+      }).then(response => {
+        vm.results = response.data.tracks.items;
+        // vm.uri = response.data.tracks.items.uri;
+        vm.searchstatusArtists = false;
+        vm.searchstatusTracks = true;
+        angular.element(document.querySelector('#searchArtists-text')).val('');
+        console.log('search result', vm.results);
+      });
+    }
+
+    function addToPlaylist(uri){
+      vm.uri = uri
+      const url = `https://api.spotify.com/v1/users/${vm.user_id}/playlists/${vm.playlistid}/tracks?uris=${vm.uri}`
+      $http
+        .post(url, vm.token)
+        .then(function(data) {
+          console.log('song data', data);
+          $http
+          .get(`https://api.spotify.com/v1/users/${vm.user_id}/playlists/${vm.playlistid}`,  { headers: {Authorization: `Bearer ${vm.token}`}
+        })
+        .then(response => {
+          console.log('playlist', response.data);
+          // vm.fullPlaylist = response.data;
+        })
+      //   .fail(function(data) {
+      //     console.log('error');
+      //   });
+      // })
+    })
+
+    }
 
 }
