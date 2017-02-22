b37c1164543cedeb2ebba665a23562caae709835
sarahemilym <sarahemendham@gmail.com>
2017-02-22 20:50:25 +0000

    only authorized rooms visable

--- src/js/controllers/rooms/index.controller.js
+++ src/js/controllers/rooms/index.controller.js
@@ -2,13 +2,44 @@ angular
 .module('MuSync')
 .controller('RoomsIndexCtrl', RoomsIndexCtrl);
 
-RoomsIndexCtrl.$inject = ['Room'];
-function RoomsIndexCtrl(Room) {
+RoomsIndexCtrl.$inject = ['Room', 'CurrentUserService'];
+function RoomsIndexCtrl(Room, CurrentUserService) {
   const vm  = this;
+  const roomArray = [];
+  const authorizedRooms = CurrentUserService.currentUser.authorized_rooms;
+  vm.authorizedRooms = [];
+
   Room
-    .query().$promise
-    .then(response => {
-      vm.rooms = response;
-      console.log(vm.rooms);
+  .query().$promise
+  .then(response => {
+    createRoomArray(response);
+
+    // const rooms = response;
+    // findMatches(authorizedRooms, roomArray);
+    // console.log('rooms', rooms);
+  });
+
+  function createRoomArray(room) {
+    room.forEach(function(room){
+      // console.log('room', room.id)
+      roomArray.push(room.id);
+      findMatches(authorizedRooms, roomArray);
     });
+  }
+
+  function findMatches(a, b){
+    console.log('a', a, 'b', b)
+
+    for (var i = 0; i < a.length; i++) {
+      for (var j = 0; j < b.length; j++) {
+        if(a[i] == b[j]){
+          if (vm.authorizedRooms.indexOf(a[i]) === -1) {
+            vm.authorizedRooms.push(a[i]);
+          }
+        }
+      }
+      console.log('authorized rooms', vm.authorizedRooms);
+      // return vm.showRooms;
+    }
+  }
 }
