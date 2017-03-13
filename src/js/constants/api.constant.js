c1e152e355193c0c6c9508cc65056007334a8840
sarahemilym <sarahemendham@gmail.com>
2017-03-13 12:53:01 +0000

    Fixing  for

--- src/js/constants/api.constant.js
+++ src/js/constants/api.constant.js
@@ -1,3 +1,3 @@
 angular
   .module('MuSync')
-  .constant('API', 'http://localhost:3000');
+  .constant('API', 'https://musyncc.herokuapp.com');
--- src/js/filters/spotify.filter.js
+++ src/js/filters/spotify.filter.js
@@ -1,7 +1,10 @@
 angular
   .module('MuSync')
-  .filter('spotifyEmbedUrl', function ($sce) {
-    return function(uri) {
-      return $sce.trustAsResourceUrl(`https://embed.spotify.com/?uri=${uri}`);
-    };
-  });
+  .filter('spotifyEmbedUrl', spotifyEmbedUrl);
+
+spotifyEmbedUrl.$inject = ['$sce'];
+function spotifyEmbedUrl($sce) {
+  return function(uri) {
+    return $sce.trustAsResourceUrl(`https://embed.spotify.com/?uri=${uri}`);
+  };
+}
